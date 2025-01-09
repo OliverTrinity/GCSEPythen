c4c57d31ecc2919d76dad2256002cb01d6dbef36
OliverTrinity <7167@trinity.croydon.sch.uk>
2025-01-09 13:06:12 +0000

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,18 @@
+#constants explained
+
+#constants look a bit like variables
+
+#but constants are declared in FULL CAPS
+
+#for example
+
+THREEPOINTSHOT = 3
+FREE_THROW = 1
+
+#When programs see a constant
+
+#It tells them to not change the value
+
+#To display the value in the constant, the code is:
+
+print(THREEPOINTSHOT)
