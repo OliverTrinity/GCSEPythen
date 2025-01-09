4b54c4c61dd232ec099ed0b3856fccf93fec9701
OliverTrinity <7167@trinity.croydon.sch.uk>
2025-01-09 12:44:05 +0000

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,14 @@
+#This is a comment
+
+#Comments start with a hash symbol
+
+#Comments are shown in a particular colour
+
+#Comments are ignored by python when executing code
+
+#Comments are used to describe code
+
+#Either line by line or a sectioin of code
+
+#comments make code more maintainable
+
